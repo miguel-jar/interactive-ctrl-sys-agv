5e25f2a8a4f25dc14631fa5a82e7e15d65b74137
miguel-jar <miguelabda90@gmail.com>
2024-08-22 00:17:14 -0300

    logica start stop + thread serial

---
+++ web_server_python/servidor_teste.py
@@ -0,0 +1,77 @@
+from flask import Flask, flash, request, redirect, render_template, jsonify
+from proc_mapa.proc_mapa import get_mapa
+from teste_serial import envia_pontos
+import threading
+import os, json
+
+paginaUpload = 'upload_mapa.html'
+paginaMapa = 'mapa.html'
+pathJson = 'web_server_python/static/config.json'
+
+UPLOAD_FOLDER = 'web_server_python/proc_mapa/mapas'
+ALLOWED_EXTENSIONS = {'dxf'}
+
+trajectory = []
+
+app = Flask(__name__)
+app.secret_key = b'miguel'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        
+        file = request.files['file']
+
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        
+        if file and allowed_file(file.filename):
+            caminhoDXF = os.path.join(app.config['UPLOAD_FOLDER'], 'mapa.DXF')
+            file.save(caminhoDXF)
+            get_mapa(caminhoDXF)
+            return redirect('/submit-trajectory')
+
+    return render_template(paginaUpload)
+
+@app.route('/submit-trajectory', methods=['POST', 'GET'])
+def submit_trajectory():
+    if request.method == 'POST':
+        data = request.json
+        trajectory = data.get('trajectory')
+
+        # Aqui você pode processar a trajetória, enviá-la ao robô, etc.
+        print("Trajetória recebida:", trajectory)
+        return jsonify({"status": "success", "trajectory": trajectory})
+    
+    return render_template(paginaMapa)
+
+@app.route('/get-configs', methods=['GET'])
+def submit_configs():
+    with open(pathJson, 'r') as configs:
+        args = json.load(configs)
+    return args
+
+@app.route('/start-stop', methods=['POST'])
+def start_stop_robot():
+    data = request.data.decode()
+    print(data)
+
+    if data == 'start':
+        return 'Robo iniciado'
+        # thread = threading.Thread(target=envia_pontos, args=(trajectory, ))
+        # thread.start()
+    elif data == 'stop':
+        return 'Robo desligado'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
