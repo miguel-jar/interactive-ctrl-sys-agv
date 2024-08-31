d45752e4b1dd76ff2ab3996d9806abe17622c6e4
daniel00nt01 <66882348+daniel00nt01@users.noreply.github.com>
2024-08-31 09:24:35 -0300

    update arduino code

--- codigos_arduino/src/MPU.cpp
+++ codigos_arduino/src/MPU.cpp
@@ -1,8 +1,14 @@
 #include "Configuracoes.h"
 
-volatile float distanciaPercorrida;
+volatile float distanciaPercorrida1;
+volatile float distanciaPercorrida2;
 
-bool controlePrescionado() {
+const float circunferenciaRoda = 11.5 * 3.14; // em CM
+const float resolucaoEncoder = 12.0;
+uint16_t contador;
+
+bool controlePrescionado()
+{
   unsigned long tiempo = pulseIn(RF_CH3, HIGH);
 
   if (tiempo > 1600.0)
@@ -11,29 +17,35 @@ bool controlePrescionado() {
     return false;
 }
 
-void interrupcaoEncoderINT1() {
-  const float circunferenciaRoda = 11.5 * 3.14;  // em CM
+void interrupcaoEncoderINT1()
+{
+  distanciaPercorrida1 += circunferenciaRoda / resolucaoEncoder;
+  contador++;
+}
+
+void interrupcaoEncoderINT2()
+{
+  const float circunferenciaRoda = 11.5 * 3.14; // em CM
   const float resolucaoEncoder = 12.0;
-  distanciaPercorrida += circunferenciaRoda / resolucaoEncoder;
+  distanciaPercorrida2 += circunferenciaRoda / resolucaoEncoder;
 }
 
-float getDistanciaPercorrida() {
-  return distanciaPercorrida;
+float getDistanciaPercorrida()
+{
+  float distancia = distanciaPercorrida1;
+  distanciaPercorrida1 = 0.0;
+  return distancia;
+  // return (distanciaPercorrida1 + distanciaPercorrida2) / 2;
 }
 
-void startGPIO() {
-
+void startGPIO()
+{
   attachInterrupt(digitalPinToInterrupt(ENCODER_INT1), interrupcaoEncoderINT1, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_INT2), interrupcaoEncoderINT2, RISING);
 
   pinMode(RF_CH3, INPUT);
-  pinMode(BUZZER, OUTPUT);
-  digitalWrite(BUZZER, LOW);
 
-  distanciaPercorrida = 0.0;
+  distanciaPercorrida1 = 0.0;
+  distanciaPercorrida2 = 0.0;
 }
 
-void tocaBuzzer() {
-  digitalWrite(BUZZER, HIGH);
-  delay(200);
-  digitalWrite(BUZZER, LOW);
-}
