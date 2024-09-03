01171ab32453d774caf1842cfe88779660926e6e
miguel-jar <miguelabdala90@gmail.com>
2024-09-03 03:27:59 +0100

    envio origem adicionado

--- web_server_python/envio_dados.py
+++ web_server_python/envio_dados.py
@@ -1,19 +1,34 @@
 import serial, time
 
+__toleranciaX, __toleranciaY = 10, 10
+
+def __organiza_coordenadas(header, x, y):
+    msbX, lsbX = (x >> 8), (x & 255)
+    msbY, lsbY = (y >> 8), (y & 255)
+
+    return header + [msbX, lsbX, msbY, lsbY]
+
 def envia_pontos(portaUSB : str, baudrate : int, data, start, stop, header, stop_flag) -> None:
     try:
         with serial.Serial(port=portaUSB, baudrate=baudrate) as cereal:
-            time.sleep(5)
+            time.sleep(6)
+
+            # Envio das coordenadas iniciais
+            x_atual, y_atual = 0, 0
+            baites = __organiza_coordenadas(header, x_atual, y_atual)
+            print("\nEnviando coordenadas iniciais!")
+            cereal.write(baites)
+
+            # Confirma o que foi enviado para o Arduino
+            xx = int.from_bytes(cereal.read(size=2))
+            yy = int.from_bytes(cereal.read(size=2))
+            print(xx, yy)
+
             cereal.write(start)
 
-            toleranciaX, toleranciaY = 10, 10
-            x_atual, y_atual = 0, 0
-
+            # Envio das coordenadas desejadas
             for x, y in data:
-                msbX, lsbX = (x >> 8), (x & 255)
-                msbY, lsbY = (y >> 8), (y & 255)
-
-                baites = header + [msbX, lsbX, msbY, lsbY]
+                baites = __organiza_coordenadas(header, x, y)
                 print("\nEnviando dados!")
                 cereal.write(baites)
 
@@ -22,7 +37,7 @@ def envia_pontos(portaUSB : str, baudrate : int, data, start, stop, header, stop
                 yy = int.from_bytes(cereal.read(size=2))
                 print(xx, yy)
 
-                while ((abs(x - x_atual) > toleranciaX) or (abs(y - y_atual) > toleranciaY)):
+                while ((abs(x - x_atual) > __toleranciaX) or (abs(y - y_atual) > __toleranciaY)):
 
                     if cereal.in_waiting > 0:
                         x_atual = int.from_bytes(cereal.read(size=2))
@@ -37,11 +52,12 @@ def envia_pontos(portaUSB : str, baudrate : int, data, start, stop, header, stop
 
                 if stop_flag.is_set():
                     print('Execucao interrompida.')
-                    cereal.write(stop)
                     break
 
             if not stop_flag.is_set():
                 print('Trajeto completo!')
+
+            cereal.write(stop)  # de qualquer forma tem que printar stop, pro carrinho parar
                 
     except serial.SerialException:
         print("\nNão foi possível comunicar com arduino. Confira a porta serial e tente novamente.")
