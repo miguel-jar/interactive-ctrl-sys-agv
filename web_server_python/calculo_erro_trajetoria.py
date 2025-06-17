be4f368e9ff73f39fccc14c838e01cf1c79b4a65
miguel-jar <miguelabdala90@gmail.com>
2025-06-17 20:06:06 -0300

    estruturado + plotagem x e y target-medido ao longo do tempo

--- web_server_python/calculo_erro_trajetoria.py
+++ web_server_python/calculo_erro_trajetoria.py
@@ -51,6 +51,36 @@ def obter_coordenadas_paths(svg_file, ids):
         
     return xxx, yyy
 
+def calcula_erro(xt, yt, xr, yr, plot = False) -> list[list[int]]:
+    o = 0
+    erros, error = [], [0] * len(xt)
+    x_target, y_target = [], []
+    
+    for f, g in zip(xr, yr):
+        dx, dy = f - xt[o], g - yt[o]
+        e = (dx ** 2 + dy ** 2) ** (1 / 2)
+        if e < 18:
+            error[o] = e ** 2 # eleva ao quadrado pra calcular o EQM
+            o = o + 1 if o < len(xt) - 1 else len(xt) - 1
+            
+        erros.append(e)
+        x_target.append(xt[o])
+        y_target.append(yt[o])
+    
+    eqm = sum(error) / len(error)
+    reqm = eqm ** (1 / 2)
+    e_medio = sum([ee ** (1 / 2) for ee in error]) / len(error)
+    
+    print("\nEQM =", eqm)
+    print("REQM =", reqm)
+    print("Erro médio absoluto =", e_medio)
+    
+    if plot:
+        plt.figure()
+        plt.plot(erros)
+    
+    return [x_target, y_target]
+
 if __name__ == "__main__":
     svg_file = "trajetos/06_10/55.svg"  # Substitua pelo caminho do seu arquivo SVG
     ids = ["line2d_25", "line2d_26"]   # Substitua pelo ID do grupo que você deseja obter
@@ -77,32 +107,33 @@ if __name__ == "__main__":
 
     xt = [(j - offset_x) * escala_x for j in xt] 
     yt = [(j - offset_y) * escala_y for j in yt]
-    plt.plot(xt, yt)
     
     xr = [(j - offset_x) * escala_x for j in xr] 
     yr = [(j - offset_y) * escala_y for j in yr]
-    plt.plot(xr, yr)
-
-    o = 0
-    erros, error = [], [0] * len(xt)
     
-    for f, g in zip(xr, yr):
-        dx, dy = f - xt[o], g - yt[o]
-        e = (dx ** 2 + dy ** 2) ** (1 / 2)
-        if e < 18:
-            error[o] = e ** 2 # eleva ao quadrado pra calcular o EQM
-            o = o + 1 if o < len(xt) - 1 else len(xt) - 1
-            
-        erros.append(e)
+    real_time = True
+    xy = not real_time
     
-    eqm = sum(error) / len(error)
-    reqm = eqm ** (1 / 2)
-    e_medio = sum([ee ** (1 / 2) for ee in error]) / len(error)
+    x_target, y_target = calcula_erro(xt, yt, xr, yr)
+    print(len(xr), len(x_target))
     
-    print("\nEQM =", eqm)
-    print("REQM =", reqm)
-    print("Erro médio absoluto =", e_medio)
+    if real_time:
+        plt.figure()
+        plt.plot(x_target)
+        plt.plot(xr)
+        plt.title("X target e X medido no tempo")
+        plt.legend(['X target', 'X medido'])
+        plt.grid()
         
-    plt.figure()
-    plt.plot(erros)
+        plt.figure()
+        plt.plot(y_target)
+        plt.plot(yr)
+        plt.title("Y target e Y medido no tempo")
+        plt.legend(['Y target', 'Y medido'])
+        plt.grid()
+    elif xy:
+        plt.figure()
+        plt.plot(xt, yt)
+        plt.plot(xr, yr)
+        
     plt.show()
